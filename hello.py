43932f05a6cbbe1473899e19e87e79c9e2ec342a
Mateus Oliveira <mateus156.2016@gmail.com>
2024-05-02 16:58:44 -0300

    novas funcionalidades em hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 #!/bin/env python3
 
-print("Ola mundo")
-print("--amend")
+#print("Ola mundo")
+#print("--amend")
 
 name = input("DIGA SEU NOME ")
-if (name == "mateus"):
-	print("OLA MESTRE")
+if ((name == "mateus") or (name == "Mateus")):
+	print(f"Seja bem vindo mestre {name} como vai")
 else:
-	print("OLA ",name)
+	print(f"Seja bem vindo {name} como vai")
 
