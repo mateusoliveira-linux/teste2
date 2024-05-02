825f900efeb47f6c87df126d54551af493f260e0
Mateus Oliveira <mateus156.2016@gmail.com>
2024-05-02 20:52:23 -0300

    novas funcionalidades em main.c

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main(){
-	int x,y;
-	printf("Ola git");
+	int n1, n2, soma;
+
+	printf("Digite um numero: ");
+	scanf("%d", &n1);
+	printf("Digite outro numero: ");
+	scanf("%d", &n2);
+
+	soma=n1+n2;
+	printf("A soma é %d", soma);
+	
 	return 0;
 }
